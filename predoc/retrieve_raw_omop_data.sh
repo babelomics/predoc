cdded770e277a3613306c94c7b402ab1a0e79fd0
Isidoro Gutierrez-Alvarez <isigalvarez@gmail.com>
2025-05-21 10:39:37 +0000

    update env variable name

--- predoc/retrieve_raw_omop_data.sh
+++ predoc/retrieve_raw_omop_data.sh
@@ -3,17 +3,17 @@
 source .env
 
 # Check if required environment variables are set
-if [ -z "$REMOTE_USER" ] || [ -z "$REMOTE_HOST" ] || [ -z "$REMOTE_RAW_OMOP_DATA" ] || [ -z "$LOCAL_DATA_DIR" ]; then
+if [ -z "$REMOTE_USER" ] || [ -z "$REMOTE_HOST" ] || [ -z "$REMOTE_RAW_OMOP_DATA" ] || [ -z "$DATA_PATH" ]; then
     echo "Error: Required personal environment variables are not set."
-    echo "Please ensure REMOTE_USER, REMOTE_HOST, REMOTE_RAW_OMOP_DATA and LOCAL_DATA_DIR are set in the environment variables."
+    echo "Please ensure REMOTE_USER, REMOTE_HOST, REMOTE_RAW_OMOP_DATA and DATA_PATH are set in the environment variables."
     exit 1
 fi
 
 # Create LOCAL_PATH directory if it doesn't exist
-mkdir -p "$LOCAL_DATA_DIR/omop/raw/"
+mkdir -p "$DATA_PATH/omop/raw/"
 
 # Run rsync command
-rsync -avz --progress "$REMOTE_USER@$REMOTE_HOST:$REMOTE_RAW_OMOP_DATA/*" "$LOCAL_DATA_DIR/omop/raw/"
+rsync -avz --progress "$REMOTE_USER@$REMOTE_HOST:$REMOTE_RAW_OMOP_DATA/*" "$DATA_PATH/omop/raw/"
 
 # Check rsync exit status
 if [ $? -eq 0 ]; then
